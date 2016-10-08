070f33d24bdf6de7843b4f346af6f9c9f00a46cb
Yuri <yuri.goncharenko@gmail.com>
2016-10-08 13:18:49 +0900

    Updates code style and naming

--- src/algo/merge-sort.clj
+++ src/algo/merge-sort.clj
@@ -1,39 +1,36 @@
 (ns algo.merge-sort)
 
-(defn- get-part-length [array]
-  (let [length (count array)]
-    (int (Math/ceil (/ length 2)))))
+(defn merge-sort [xs]
+  (let [split-xs (split-vector xs)]
+    (if (<= (count xs) 1)
+      xs
+      (vec (merge (merge-sort (get split-xs 0)) (merge-sort (get split-xs 1)))))))
 
-(defn split-array [array]
-  (if (= (count array) 1)
-    array
-    (let [part-length (get-part-length array)]
-      (conj [] (subvec array 0 part-length) (subvec array part-length (count array))))))
+(defn- split-vector [xs]
+  (let [n (count xs)]
+    (if (= n 1)
+      xs
+      (let [part-length (get-part-length xs)]
+        (conj [] (subvec xs 0 part-length) (subvec xs part-length n))))))
+
+(defn- get-part-length [xs]
+  (let [n (count xs)]
+    (int (Math/ceil (/ n 2)))))
+
+(defn- merge [first-half second-half]
+  (let [output []]
+    (loop [current-output       output
+           left-part-remainder  first-half
+           right-part-remainder second-half]
+      (cond
+        (nil? (first left-part-remainder)) (concat current-output right-part-remainder)
+        (nil? (first right-part-remainder)) (concat current-output left-part-remainder)
+        :else
+        (if (left-smaller? left-part-remainder right-part-remainder)
+          (recur (conj current-output (first left-part-remainder)) (rest left-part-remainder) right-part-remainder)
+          (recur (conj current-output (first right-part-remainder)) left-part-remainder (rest right-part-remainder)))))))
 
 (defn- left-smaller? [left-part right-part]
   (< (first left-part) (first right-part)))
 
-(defn- merge [first-half second-half]
-  (let [output-length (+ (count first-half) (count second-half))
-        output        []]
-    (loop [current-output       output
-           left-part-remainder  first-half
-           right-part-remainder second-half]
-      (if (= (count current-output) output-length)
-        current-output
-          (cond
-            (nil? (first left-part-remainder)) (concat current-output right-part-remainder)
-            (nil? (first right-part-remainder)) (concat current-output left-part-remainder)
-            :else
-            (if (left-smaller? left-part-remainder right-part-remainder)
-              (recur (conj current-output (first left-part-remainder)) (rest left-part-remainder) right-part-remainder)
-              (recur (conj current-output (first right-part-remainder)) left-part-remainder (rest right-part-remainder))))))))
-
-(defn merge-sort [input-array]
-  (let [splited-array (split-array input-array)]
-    (if (<= (count input-array) 1)
-      input-array
-      (vec (merge (merge-sort (get splited-array 0)) (merge-sort (get splited-array 1)))))))
-
-(merge-sort [0 89 0 2 0 89 1 8 77 212 9 5.4 89])
 
