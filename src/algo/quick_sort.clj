80a68fd5065247570e646a6160c670368915a601
Yuri <yuri.goncharenko@gmail.com>
2016-10-13 23:27:19 +0900

    Implements comparison counting for quicksort

--- src/algo/quick_sort.clj
+++ src/algo/quick_sort.clj
@@ -1,24 +1,60 @@
-(ns algo.quick-sort)
+(ns algo.quick-sort
+  (:require [clojure.java.io :as io]))
 
-(defn partition [xs pivot]
-  (let [pivot-element (get xs pivot)
+(defn- partition-in-place [xs l r]
+  (let [pivot-element (get xs l)
         n (count xs)]
-    (loop [leftmost []
-           rightmost []
-           i 0]
-      (if (= i n)
-        (conj [] leftmost rightmost)
-        (if (= i pivot)
-          (recur leftmost rightmost (inc i))
-          (if (< (get xs i) pivot-element)
-            (recur (conj leftmost (get xs i)) rightmost (inc i))
-            (recur leftmost (conj rightmost (get xs i)) (inc i))))))))
+    (loop [i (inc l)
+           j (inc l)
+           current-partition xs]
+      (if (= j r)
+        (let [final-partition (swap current-partition l (dec i))]
+          [(subvec final-partition l (dec i)) (subvec final-partition i n)])
+        (if (< (get xs j) pivot-element)
+          (recur (inc i) (inc j) (swap current-partition j i))
+          (recur i (inc j) current-partition))))))
 
-(defn quick-sort [xs pivot]
+(defn- swap [xs i j]
+  (-> xs
+      (assoc i (xs j))
+      (assoc j (xs i))))
+
+(defn quick-sort [xs pivot total-comparisons]
   (if (<= (count xs) 1)
-    xs
-    (let [[left-part, right-part] (partition xs pivot)]
-      (concat (quick-sort left-part pivot) [(get xs pivot)] (quick-sort right-part pivot)))))
+    {:elements xs :comparisons total-comparisons}
+    (let [[left-part, right-part] (partition-in-place xs pivot (count xs))
+          left-part-sorted        (quick-sort left-part 0 (count left-part))
+          right-part-sorted       (quick-sort right-part 0 (count right-part))]
+      {:elements    (concat
+                     (:elements left-part-sorted)
+                     [(get xs pivot)]
+                     (:elements right-part-sorted))
+       :comparisons (+ total-comparisons (:comparisons left-part-sorted) (:comparisons right-part-sorted))})))
 
-(quick-sort [3 9 8 4 6 10 2 5 7 1] 0)
+(defn- file->vector [file-name]
+  (let [reader (io/reader file-name)]
+    (map #(Integer. %) (line-seq reader))))
+
+(def ^{:private true} test-cases [{:path   "./resources/quick_sort_10.txt"
+                                   :output 25}
+                                  {:path   "./resources/quick_sort_100.txt"
+                                   :output 615}
+                                  {:path   "./resources/quick_sort_1000.txt"
+                                   :output 10297}])
+
+(defn count-comparisons
+  ([file-name]
+   (let [xs (file->vector file-name)]
+     (quick-sort (vec xs) 0 0)))
+  ([file-name n]
+   (let [xs (take n (file->vector file-name))]
+     (quick-sort (vec xs) 0 0))))
+
+(defn- run-tests [tests]
+  (for [test tests
+        :let [{path :path
+               output :output} test]]
+    (assert (= output (:comparisons (count-comparisons path))))))
+
+(run-tests test-cases)
 
