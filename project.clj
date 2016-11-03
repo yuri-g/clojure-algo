0a8e2c6f20a9dca3993600175c615cc853adfe8d
yuri <yuri.goncharenko@gmail.com>
2016-11-03 19:52:49 +0900

    Add kibit plugin

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-kibit "0.1.2"]]
   :main algo.core
   :aot [algo.core])
