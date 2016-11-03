fcc2bc6a07a2960b9cc7c29e7e0d0dc5f428fa43
Yuri <yuri.goncharenko@gmail.com>
2016-11-03 13:43:02 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject algo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject algo "0.1.0"
+  :description "Various algorithms implemented in Clojure."
+  :url "https://github.com/yuri-g/clojure-algo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.int-map "0.2.4"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main algo.core
   :aot [algo.core])
