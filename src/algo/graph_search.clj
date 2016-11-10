0c9ca5706e3772273a11a3aba97a61baf77f3716
yuri <yuri.goncharenko@gmail.com>
2016-11-10 22:51:42 +0900

    Uses vertices instead of edges, some progess on DFS

--- src/algo/graph_search.clj
+++ src/algo/graph_search.clj
@@ -2,61 +2,47 @@
   (:require [clojure.spec :as s]
             [clojure.spec.test :as stest]))
 
-(s/def ::head integer?)
-(s/def ::tail integer?)
 (s/def ::is-explored boolean?)
 
-(s/def ::edge (s/keys :req [::head] :opt [::tail]))
+(s/def ::heads (s/coll-of integer? :kind vector?))
+(s/def ::vertex (s/keys :req [::heads ::is-explored]))
 
-;TODO those are not edges, but vertices + edges, rename!
-(s/def ::edges (s/map-of integer? ::edge :conform-keys true))
+(s/def ::vertices (s/map-of integer? ::vertex :conform-keys true))
 
-(s/def ::explorable-edge (s/keys :req [::head ::is-explored] :opt [::tail]))
+(defn- mark-explored [vertex]
+  (assoc vertex ::is-explored true))
 
-;TODO those are not edges, but vertices + edges, rename!
-; also, map will not do, since keys have to be unique
-; store in map, but keep array of all edges per vertex?
-(s/def ::explorable-edges (s/map-of integer? ::explorable-edge :conform-keys true))
+(defn- explore [vertex vertices]
+  (assoc vertices vertex (mark-explored vertex)))
 
-(defn- edges->explorable-edges [edges]
-  (reduce (fn [explorable-edges [vertex edge]] (assoc explorable-edges vertex (assoc edge ::is-explored false))) {} edges))
+(defn- get-edges-for-vertex [current-vertex vertices]
+  (let [{heads ::heads} current-vertex]
+    (reduce (fn [vertex-edges head]
+              (if (contains? vertices head)
+                (assoc vertex-edges head (get vertices head))
+                vertex-edges))
+            {}
+            heads)))
 
-(defn- mark-explored [edge]
-  (assoc edge ::is-explored true))
+(defn depth-first-search [vertices current-vertex]
+  (let [explored-vertices (explore current-vertex vertices)
+        current-explored-vertex (get explored-vertices current-vertex)
+        edges (get-edges-for-vertex current-explored-vertex explored-vertices)]
+    (if (empty edges)
+      explored-vertices
+      (for [edge edges]
+        (if (not (::is-explored? edge))
+          (depth-first-search explored-vertices (get explored-vertices edge)))))))
 
-(defn- explore [graph edge]
-  (assoc graph edge (mark-explored edge)))
+(def test-graph {1 {::heads [2, 3] ::is-explored false}
+                 2 {::heads [3] ::is-explored false}
+                 3 {::heads [] ::is-explored false}})
 
-(defn- get-edges-for-vertex [current-vertex graph]
-  (reduce (fn [edges [vertex edge]]
-            (if (= vertex current-vertex)
-              (assoc edges (::head edge) edge)
-              edges))
-          {}
-          graph))
-
-(get-edges-for-vertex 1 test-graph)
-
-(defn depth-first-search [edges start-vertex]
-  (let [explorable-edges (edges->explorable-edges edges)]
-    (loop [already-explored explorable-edges
-           current-edge (get already-explored start-vertex)
-           current-edges (explore already-explored (::head current-edge))])))
-
-;TODO reverse head<->tail
-(def test-graph {1 {::head 2}
-                 2 {::head 3}
-                 3 {::head 1}})
-
-(explore test-graph (get test-graph 1))
+(depth-first-search test-graph (get test-graph 2))
 
 (s/fdef depth-first-search
-        :args (s/cat :edges ::edges
-                     :starting-node integer?)
-        :ret ::explorable-edges)
-
-(s/fdef edges->explorable-edges
-        :args (s/cat :edges ::edges)
+        :args (s/cat :vertices ::vertices
+                     :starting-vertex integer?)
         :ret ::explorable-edges)
 
 (s/fdef mark-explored
@@ -64,11 +50,10 @@
         :ret ::explorable-edge)
 
 (s/fdef explore
-        :args (s/cat :graph ::explorable-edges
-                     :edge ::explorable-edge))
+        :args (s/cat :current-vertex ::vertex
+                     :vertices ::vertices))
 
 (stest/check `depth-first-search)
-(stest/check `edges->explorable-edges)
 (stest/check `mark-explored)
 (stest/check `explore)
 
